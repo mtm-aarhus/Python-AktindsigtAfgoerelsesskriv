f3daed31ffdc124c3c30ac66251200a2bb927211
Laura-Salmonsen <balas@aarhus.dk>
2025-05-14 14:43:55 +0200

    raw_reason

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -326,6 +326,7 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
         for docs in results_dict.values():
             for doc in docs:
                 if doc["decision"] in ["Nej", "Delvis"] and doc["reason"] and str(doc["reason"]).strip():
+                    reason_raw = doc.get("reason")
                     if isinstance(reason_raw, float) and math.isnan(reason_raw):
                         orchestrator_connection.log_info('Ingen begrundelse valgt')
                         reason = 'Intet valgt'
